b570bf44cc5556d2ade6b8923033cbca8fd23f4c
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2022-03-30 14:59:41 +0300

    scaffold

---
+++ library_app/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+#  import python files at the module top level and import the subdirectories also containing python files.
+
+from . import controllers
+from . import models
